e1021b30706ac0e58374c0701035a08d5fb418a1
begimay <begi.asanbekova@gmail.com>
2018-03-11 14:21:59 +0600

    f

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+//
+// Подключение библиотек
+//
+// --------------------------------------------
+
+//= jquery/jquery.js
+//= uikit/uikit.min.js
+//= uikit/uikit-icons.min.js
+//= slick/slick.min.js
+
+
+$(document).ready(function () {
+
+});
